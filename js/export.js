91460096c4a7e639010211192ecfc11e5ef7f712
ouhouhsami <samuel.goldszmidt@gmail.com>
2014-10-19 14:45:05 +0200

    angular version first iteration

--- js/export.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var items = localStorage.length;
-	var obj = {};
-	for (var i=0;i<items;i++) {
-		var value = localStorage.key(i);
-		obj[value] = localStorage.getItem(value);
-	}
-	$('#export').val(JSON.stringify(obj));
-});
--- js/import.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#import-btn').click(function(e){
-		var obj = JSON.parse($('#import').val());
-		for(var key in obj){
-			//console.log(key, obj[key]);
-			localStorage.setItem(key, obj[key]);
-		};
-	});
-});
